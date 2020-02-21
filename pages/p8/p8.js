b648f665ff8b71c544b3dcd1106719f3e800f422
Pride Leong <lykling.lyk@gmail.com>
2020-02-21 17:45:50 +0800

    Copy pages
    Signed-off-by: Pride Leong <lykling.lyk@gmail.com>

---
+++ pages/p8/p8.js
@@ -0,0 +1,66 @@
+import agl from 'angelia-swan';
+Page({
+    data: {
+
+    },
+    onLoad: function () {
+        // 监听页面加载的生命周期函数
+    },
+    onReady: function() {
+        // 监听页面初次渲染完成的生命周期函数
+    },
+    onShow: function() {
+        // 监听页面显示的生命周期函数
+    },
+    onHide: function() {
+        // 监听页面隐藏的生命周期函数
+    },
+    onUnload: function() {
+        // 监听页面卸载的生命周期函数
+    },
+    onPullDownRefresh: function() {
+        // 监听用户下拉动作
+    },
+    onReachBottom: function() {
+        // 页面上拉触底事件的处理函数
+    },
+    onShareAppMessage: function () {
+        // 用户点击右上角转发
+    },
+    formSubmit: function (e) {
+        console.log(`submitting form: ${JSON.stringify(e.detail.value, null, 4)}`);
+        const cvtype = Number(e.detail.value.cvtype);
+        if (!isNaN(cvtype) && cvtype > 0) {
+            swan.request({
+                url: 'http://fclog.baidu.com/log/test',
+                method: 'POST',
+                data: '{"actid": "xxx", "item": "candies", "amount": 20}',
+                header: {
+                    'Content-Type': 'application/json; charset=utf-8'
+                },
+                success: res => {
+                    // 在请求成功的回调方法里回传转化数据
+                    agl.log({
+                        convertType: cvtype
+                    });
+                    swan.showToast({
+                        title: `转化成功: ${cvtype}`
+                    })
+                },
+                fail: err => {
+                    swan.showToast({
+                        title: "服务异常"
+                    });
+                }
+            });
+        }
+        else {
+            swan.showToast({
+                title: '无效的转化类型'
+            });
+        }
+    },
+    formReset: function () {
+        console.log('reset form');
+    }
+});
